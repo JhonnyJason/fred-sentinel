17696568a7ef0dc31e58e39324590c4780c18699
Lenard Frommelt <lenny09918050@gmail.com>
2026-04-03 13:22:12 +0300

    thingysync push

--- testing/test.mjs
+++ testing/test.mjs
@@ -1,8 +1,12 @@
 import * as tested from "../output/index.js"
 
+import fs from "node:fs"
+
+const apiKey = fs.readFileSync("key.txt", {encoding: "utf8"})
+
 async function run() {
 
-    //run test.
+    console.log(apiKey)
 }
 
 run()
